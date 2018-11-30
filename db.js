968551f3a7ba878751a6fdb1a2c48e63e681fb4f
danGRT <d.geraghty06@gmail.com>
2018-11-30 14:43:54 +0000

    initial commit

---
+++ db.js
@@ -0,0 +1,46 @@
+//mockDB
+
+const STUDENTS = [
+  { id: 1, name: "Dan", hasGraduated: true },
+  { id: 2, name: "Laura", hasGraduated: true },
+  { id: 3, name: "John", hasGraduated: false },
+  { id: 4, name: "Jenny", hasGraduated: true },
+  { id: 5, name: "Stuart", hasGraduated: true },
+  { id: 6, name: "Yusuf", hasGraduated: false },
+  { id: 7, name: "Joe", hasGraduated: true },
+  { id: 8, name: "Amber", hasGraduated: true },
+  { id: 9, name: "Nicole", hasGraduated: false },
+  { id: 10, name: "Bamdad", hasGraduated: true },
+  { id: 11, name: "Steve", hasGraduated: true },
+  { id: 12, name: "Felicia", hasGraduated: false }
+];
+
+const PROJECTS = [
+  { id: 1, name: "Food App", studentIds: [1, 2] },
+  { id: 2, name: "Finance App", studentIds: [3, 4] },
+  { id: 3, name: "Pet App", studentIds: [5, 6] },
+  { id: 4, name: "Barber App", studentIds: [7, 8] },
+  { id: 5, name: "Charity App", studentIds: [9, 10] },
+  { id: 6, name: "Meme App", studentIds: [11, 12] }
+];
+
+const COHORTS = [
+  { id: 1, studentIds: [1, 2, 3, 4], projectIds: [1, 2] },
+  { id: 2, studentIds: [5, 6, 7, 8], projectIds: [3, 4] },
+  { id: 3, studentIds: [9, 10, 11, 12], projectIds: [5, 6] },
+  { id: 4, studentIds: [5, 6, 7, 8], projectIds: [3, 4] }
+];
+
+const getAllStudents = () => STUDENTS;
+const getStudentById = id => STUDENTS.find(student => student.id === id);
+
+const getAllProjects = () => PROJECTS;
+const getProjectsByStudent = studentId =>
+  PROJECTS.filter(project => project.studentIds.includes(studentId));
+
+module.exports = {
+  getAllStudents,
+  getStudentById,
+  getAllProjects,
+  getProjectsByStudent
+};
